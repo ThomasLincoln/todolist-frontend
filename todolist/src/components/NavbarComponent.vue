55b4ce5ea87beec9ccc3370abc2dc0df5ea83189
Thomas Lincoln <thmas5952@gmail.com>
2024-07-17 11:17:19 -0300

    feat: :art: finish style changes

--- todolist/src/components/NavbarComponent.vue
+++ todolist/src/components/NavbarComponent.vue
@@ -1,9 +1,40 @@
 <template>
   <div class="navbar bg-base-100">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">TodoBook</a>
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
+          </svg>
+        </div>
+        <ul tabindex="0" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+          <li>
+            <RouterLink to="/">Inicio</RouterLink>
+          </li>
+          <li>
+            <a>em construção</a>
+          </li>
+          <li>
+            <RouterLink to="/contato">Contato</RouterLink>
+          </li>
+        </ul>
+      </div>
+      <RouterLink to="/" class="btn btn-ghost text-xl">TodoBook</RouterLink>
     </div>
-    <div class="flex-none gap-2">
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <RouterLink to="/">Inicio</RouterLink>
+        </li>
+        <li>
+          <a>em construção</a>
+        </li>
+        <li>
+          <RouterLink to="/contato">Contato</RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div class="navbar-end">
       <div class="form-control">
         <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
       </div>
